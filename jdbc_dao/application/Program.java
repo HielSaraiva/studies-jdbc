12e7e4c8725f7c623b4c37d35b67562c84d84298
Hiel Saraiva Freitas de Queiroga <hielsaraiva11.hs@gmail.com>
2024-05-31 20:40:07 -0300

    update implementation

--- jdbc_dao/application/Program.java
+++ jdbc_dao/application/Program.java
@@ -38,6 +38,12 @@ public class Program {
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id: " + newSeller.getId());
 
+        System.out.println("\n\n===== TEST 5: seller update =====");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Waine");
+        sellerDao.update(seller);
+        System.out.println("Updated Completed!");
+
         DB.closeConnection();
     }
 }
